a2a4d9a136195db4640f02ad4ff38d0289d8a6f7
Todor Todorov <todor@todorov.bg>
2021-01-29 16:14:40 +0200

    Initial commit

---
+++ src/nuauth/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nuauth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
